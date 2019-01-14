ccc3d49054d073534dba0dba49c1c7ca89520238
林佳志乃 <twinklestaryoshino@gmail.com>
2019-01-15 01:30:01 +0900

    change current_user to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,5 @@ class ApplicationController < ActionController::Base
   include Banken
   before_action :authenticate_user!
 
-  def current_user
-  if params[:user_id].blank?
-    current_user
-  else
-    User.find(params[:user_id])
-  end
-end
+
 end
--- app/loyalties/blogs_loyalty.rb
+++ app/loyalties/blogs_loyalty.rb
@@ -1,5 +1,5 @@
 class BlogsLoyalty < ApplicationLoyalty
   def edit?
-    current_user.admin? || current_user == record.user
+    user.admin? || user == record.user
   end
 end
